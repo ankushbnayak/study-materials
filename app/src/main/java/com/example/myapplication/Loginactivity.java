5ef14726470a8dab6a530d813b022edf1c7d28f2
adityakamath16 <61751718+adityakamath16@users.noreply.github.com>
2020-04-25 13:52:43 +0530

    Major bugs fixed and added navigation drawer

--- app/src/main/java/com/example/myapplication/Loginactivity.java
+++ app/src/main/java/com/example/myapplication/Loginactivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,15 +16,19 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Loginactivity extends AppCompatActivity {
     public EditText email;
     public  EditText pass;
     public Button login;
     public FirebaseAuth mfire;
+    FirebaseUser user;
     public TextView signup;
     public TextView forgot;
 
+    //for progressbar
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,20 +41,26 @@ public class Loginactivity extends AppCompatActivity {
         login =findViewById(R.id.button);
         mfire=FirebaseAuth.getInstance();
 
+        progressBar = findViewById(R.id.login_progress);
+        progressBar.setVisibility(View.GONE);
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
                 String emailid=email.getText().toString();
                 String password=pass.getText().toString();
                 if(emailid.isEmpty())
                 {
                     email.setError("Email is empty");
                     email.requestFocus();
+                    progressBar.setVisibility(View.GONE);
                 }
                 else if(password.isEmpty())
                 {
                     pass.setError("Password is empty");
                     pass.requestFocus();
+                    progressBar.setVisibility(View.GONE);
                 }
                 else if(!(emailid.isEmpty()&&password.isEmpty()))
                 {
@@ -63,10 +74,13 @@ public class Loginactivity extends AppCompatActivity {
                             Toast.makeText(Loginactivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
                             Intent intent= new Intent(Loginactivity.this,Navigate.class);
                             startActivity(intent);
+                            progressBar.setVisibility(View.GONE);
+                            finish();
                         }
                         else if(!(task.isSuccessful()))
                         {
                             Toast.makeText(Loginactivity.this,"Login not successfull",Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
                         }
 
                     }
@@ -93,5 +107,18 @@ public class Loginactivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        user = mfire.getCurrentUser();
+        if(user != null)
+        {
+            Intent sign= new Intent(Loginactivity.this,Navigate.class);
+            startActivity(sign);
+            finish();
+
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/Navigate.java
+++ app/src/main/java/com/example/myapplication/Navigate.java
@@ -1,16 +1,32 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class Navigate extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Navigate extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     public Button upload;
     public Button download;
     public Button logout;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+
+
+    //for navigation drawer
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +34,17 @@ public class Navigate extends AppCompatActivity {
         upload= findViewById(R.id.button3);
         download=findViewById(R.id.button4);
         logout=findViewById(R.id.button5);
+
+
+        mDrawerLayout =(DrawerLayout)findViewById(R.id.drawer);
+
+        mToggle = new ActionBarDrawerToggle(Navigate.this,mDrawerLayout,//toolbar,
+                R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationview = (NavigationView) findViewById(R.id.navigation_view);
+        navigationview.setNavigationItemSelectedListener(this);
 
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,4 +68,68 @@ public class Navigate extends AppCompatActivity {
             }
         });
     }
+
+
+    //when the navigation drawer is clicked and this is used for toggling the event
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+    {
+        int id = menuItem.getItemId();
+        if(id == R.id.download_notes)
+        {
+            Intent download = new Intent(Navigate.this,downloadactivity.class);
+            startActivity(download);
+            return true;
+        }
+        else if(id == R.id.upload_notes)
+        {
+            Intent upload = new Intent(Navigate.this,uploadactivity.class);
+            startActivity(upload);
+            return true;
+        }
+        else if(id == R.id.about_us)
+        {
+            //add your own intent or anything else
+            return true;
+        }
+
+        else if(id == R.id.rate_us)
+        {
+            //add your own intent or feature but remember to change in the menu
+            return true;
+        }
+        else if(id == R.id.sign_out)
+        {
+            firebaseAuth = FirebaseAuth.getInstance();
+            firebaseAuth.signOut();
+            Intent signout = new Intent(Navigate.this,Loginactivity.class);
+            startActivity(signout);
+            finish();
+        }
+        return false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        if (user == null)
+        {
+            //incase if the user is not signed in
+            Intent notsignedin = new Intent(Navigate.this,Loginactivity.class);
+            startActivity(notsignedin);
+            finish();
+        }
+    }
+
 }
