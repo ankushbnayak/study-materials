a4f2dcfa4fa8bac7940555ce6e4270c939344a3b
ankushbnayak <bankushnayak@gmail.com>
2020-04-26 20:16:19 +0530

    Okay

--- app/src/main/java/com/args/StudyMaterials/uploadactivity.java
+++ app/src/main/java/com/args/StudyMaterials/uploadactivity.java
@@ -15,6 +15,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +23,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
@@ -33,8 +36,10 @@ public class uploadactivity extends AppCompatActivity {
    public  Button choose;
    public Button upload;
    public TextView text;
+   public Switch aSwitch;
    public FirebaseStorage fstore;
    public FirebaseDatabase fdb;
+   public FirebaseAuth firebaseAuth;
 
    ProgressDialog pd;
    Uri pdfuri;
@@ -52,6 +57,7 @@ public class uploadactivity extends AppCompatActivity {
         choose=findViewById(R.id.button6);
         upload=findViewById(R.id.button7);
         text=findViewById(R.id.textView5);
+        aSwitch=findViewById(R.id.account_type_switch);
 
         branch = findViewById(R.id.name_of_branch);
         notes_name = findViewById(R.id.name_of_notes);
@@ -90,6 +96,10 @@ public class uploadactivity extends AppCompatActivity {
                 {
                     branch.setError("please enter your branch");
                     branch.requestFocus();
+                }
+                else if(aSwitch.isChecked())
+                {
+                    uploadfileprivately(pdfuri);
                 }
                 else
                 {
@@ -163,6 +173,72 @@ public class uploadactivity extends AppCompatActivity {
 
     }
 
+    private void uploadfileprivately(Uri pdfuri)
+    {
+        final String branch_name = branch.getText().toString();
+        final String notes_selected_name = notes_name.getText().toString();
+        pd=new ProgressDialog(this);
+        pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        pd.setTitle("Uploading file....");
+        pd.setProgress(0);
+        pd.show();
+        String path = pdfuri.getPath();
+        String filename = path.substring(path.lastIndexOf("/")+1);
+        final String fileName;
+        if (filename.indexOf(".") > 0) {
+            fileName = filename.substring(0, filename.lastIndexOf("."));
+        } else {
+            fileName =  filename;
+        }
+        //final String fileName=System.currentTimeMillis()+"";
+        StorageReference storageReference=fstore.getReference();//get path where file will be stored in firebase
+        storageReference.child("Uploads").child(fileName).putFile(pdfuri)//make sub directory to store the files
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        String url=taskSnapshot.getMetadata().getReference().getDownloadUrl().toString(); //PROBLEM
+                        firebaseAuth=FirebaseAuth.getInstance();
+                        FirebaseUser user_branch=firebaseAuth.getCurrentUser();
+                        String user=user_branch.getUid();
+                        DatabaseReference databaseReference=fdb.getReference();//path to root
+
+                        notes.setNotes_url(fileName);
+                        notes.setNotes_branch(branch_name);
+                        notes.setNotes_name(notes_selected_name);
+                        databaseReference = FirebaseDatabase.getInstance().getReference(user);
+                        databaseReference.push().setValue(notes).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful())
+                                {
+                                    Toast.makeText(uploadactivity.this,"File successfully uploaded",Toast.LENGTH_SHORT).show();
+                                }
+                                else if(!(task.isSuccessful()))
+                                {
+                                    Toast.makeText(uploadactivity.this,"File upload unsuccessful2",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(uploadactivity.this,"File upload unsuccessful",Toast.LENGTH_SHORT).show();
+
+                    }
+                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                int currentProgress=(int) (100*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                pd.setProgress(currentProgress);
+
+
+
+            }
+        });
+
+    }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
